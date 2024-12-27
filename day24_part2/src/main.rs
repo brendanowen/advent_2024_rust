2ff49f3e657fcb4936e7145a8132d75979fa067a
Brendan Owen <brendanowen@gmail.com>
2024-12-27 22:45:50 +0000

    Day 24 part 2

--- day24_part2/src/main.rs
+++ day24_part2/src/main.rs
@@ -105,11 +105,122 @@ fn parse_file(
     Ok((values, rules))
 }
 
+fn build_order(
+    values: &mut HashMap<[char; 3], Option<bool>>,
+    rules: &Vec<Rule>,
+    number_bits: usize,
+) -> Vec<Rule> {
+    clear_values(values, number_bits);
+    let mut order: Vec<Rule> = Vec::new();
+    let mut changed = true;
+    while changed {
+        changed = false;
+        for rule in rules {
+            let input1 = values.get(&rule.input1).unwrap();
+            let input2 = values.get(&rule.input2).unwrap();
+            let output = values.get(&rule.output).unwrap();
+            if input1.is_some() && input2.is_some() && output.is_none() {
+                changed = true;
+                let value1 = input1.unwrap();
+                let value2 = input2.unwrap();
+                let calculated = match rule.operator {
+                    Operator::AND => value1 && value2,
+                    Operator::OR => value1 || value2,
+                    Operator::XOR => value1 ^ value2,
+                };
+
+                *values.get_mut(&rule.output).unwrap() = Some(calculated);
+                order.push(rule.clone());
+            }
+        }
+    }
+
+    order
+}
+
+fn calculate_value(
+    values: &mut HashMap<[char; 3], Option<bool>>,
+    rules: &Vec<Rule>,
+    bits: usize,
+    swaps: &HashMap<[char; 3], [char; 3]>,
+) -> Vec<Option<usize>> {
+    let mut changed = true;
+    while changed {
+        changed = false;
+        for rule in rules {
+            let input1 = values.get(&rule.input1).unwrap();
+            let input2 = values.get(&rule.input2).unwrap();
+            let output = values.get(&rule.output).unwrap();
+            if input1.is_some() && input2.is_some() {
+                let value1 = input1.unwrap();
+                let value2 = input2.unwrap();
+                let calculated = match rule.operator {
+                    Operator::AND => value1 && value2,
+                    Operator::OR => value1 || value2,
+                    Operator::XOR => value1 ^ value2,
+                };
+
+                let new_output = swaps.get(&rule.output);
+
+                if new_output.is_none() {
+                    if output.is_none() {
+                        changed = true;
+                        *values.get_mut(&rule.output).unwrap() = Some(calculated);
+                    }
+                } else {
+                    let other_output = values.get(new_output.unwrap()).unwrap();
+                    if other_output.is_none() {
+                        changed = true;
+                        *values.get_mut(new_output.unwrap()).unwrap() = Some(calculated);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut bit_values: Vec<Option<usize>> = vec![None; bits];
+    for value in values {
+        if value.0[0] == 'z' {
+            let num_str = String::from_iter(value.0[1..].iter());
+            let test = num_str.parse::<usize>().ok().unwrap();
+            bit_values[test] = if value.1.is_none() {
+                None
+            } else {
+                Some(if value.1.unwrap() { 1 } else { 0 })
+            };
+        }
+    }
+    bit_values
+}
+
+fn clear_values(values: &mut HashMap<[char; 3], Option<bool>>, bits: usize) {
+    for value in &mut *values {
+        *value.1 = None;
+    }
+    for i in 0..bits {
+        let x: [char; 3] = [
+            'x',
+            char::from_digit((i / 10) as u32, 10).unwrap(),
+            char::from_digit((i % 10) as u32, 10).unwrap(),
+        ];
+        if let Some(val) = values.get_mut(&x) {
+            *val = Some(false);
+        }
+        let y: [char; 3] = [
+            'y',
+            char::from_digit((i / 10) as u32, 10).unwrap(),
+            char::from_digit((i % 10) as u32, 10).unwrap(),
+        ];
+        if let Some(val) = values.get_mut(&y) {
+            *val = Some(false);
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (mut values, mut rules) = parse_file("data.txt")?;
+    let (mut values, rules) = parse_file("data.txt")?;
 
-    let bits = values.len() / 2;
-
+    let number_bits: usize = values.len() / 2;
     for rule in &rules {
         if !values.contains_key(&rule.input1) {
             values.insert(rule.input1, None);
@@ -122,123 +233,118 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let mut outputs: Vec<Option<Rule>> = vec![None; bits];
+    let mut outputs: Vec<[char; 3]> = vec![];
     for rule in &rules {
-        if rule.output[0] != 'z' {
-            continue;
+        outputs.push(rule.output.clone());
+    }
+    let number_outputs = outputs.len();
+
+    let mut swap_combinations: Vec<HashMap<[char; 3], [char; 3]>> = vec![];
+    for i in 0..number_outputs {
+        for j in (i + 1)..number_outputs {
+            let mut hash_map: HashMap<[char; 3], [char; 3]> = HashMap::new();
+            hash_map.insert(outputs[i], outputs[j]);
+            hash_map.insert(outputs[j], outputs[i]);
+            swap_combinations.push(hash_map);
         }
-        if rule.operator != Operator::XOR {
-            continue;
-        }
-        let output_str = String::from_iter(rule.output[1..].iter());
-        let value = output_str.parse::<usize>().ok();
-        if value.is_none() {
-            continue;
-        }
-        let index = value.unwrap();
-        outputs[index] = Some(rule.clone());
     }
 
-    let mut input_ands: Vec<Option<Rule>> = vec![None; bits];
-    let mut input_xors: Vec<Option<Rule>> = vec![None; bits];
-    for rule in &rules {
-        let num_str1 = String::from_iter(rule.input1[1..].iter());
-        let value1 = num_str1.parse::<usize>().ok();
-        if value1.is_none() {
-            continue;
-        }
-        let num_str2 = String::from_iter(rule.input2[1..].iter());
-        let value2 = num_str2.parse::<usize>().ok();
-        if value2.is_none() {
-            continue;
-        }
-        let index1: usize = value1.unwrap();
-        let index2: usize = value2.unwrap();
-        if index1 != index2 {
-            continue;
-        }
-        if rule.input1[0] != 'x' && rule.input1[0] != 'y' {
-            continue;
-        }
-        if rule.input2[0] != 'x' && rule.input2[0] != 'y' {
-            continue;
-        }
-        if rule.input1[0] == rule.input2[0] {
-            continue;
-        }
+    let order = build_order(&mut values, &rules, number_bits);
 
-        if rule.operator == Operator::AND {
-            input_ands[index1] = Some(rule.clone());
-        } else if rule.operator == Operator::XOR {
-            input_xors[index2] = Some(rule.clone());
-        }
-    }
-    let mut swaps: Vec<[char; 3]> = vec![];
-    let total_rules = rules.len();
-    let mut problems: Vec<bool> = vec![false; bits + 1];
-    for i in 0..bits {
-        if outputs[i].is_none() {
-            let output = input_xors[i].clone().unwrap().output;
-            let mut index1: usize = 0;
-            let mut index2: usize = 0;
+    for i in 0..(number_bits - 2) {
+        println!("{}", i);
+        let hash_map: HashMap<[char; 3], [char; 3]> = HashMap::new();
+        let base_differences = all_differences(&hash_map, number_bits, &mut values, &order, i);
 
-            for rule_index in 0..total_rules {
-                let rule: &Rule = &rules[rule_index];
-                if rule.operator != Operator::XOR {
-                    continue;
-                }
-                if output == rule.input1 || output == rule.input2 {
-                    index1 = rule_index;
-                    break;
+        if base_differences != 0 {
+            for swaps in &swap_combinations {
+                let differences = all_differences(swaps, number_bits, &mut values, &order, i);
+
+                if differences == 0 {
+                    println!("{:?}", swaps);
                 }
             }
-            for rule_index in 0..total_rules {
-                let rule2 = &rules[rule_index];
-                if rule2.output[0] != 'z' {
-                    continue;
-                }
-                if rule2.operator != Operator::XOR {
-                    continue;
-                }
-                let output_str = String::from_iter(rule2.output[1..].iter());
-                let value = output_str.parse::<usize>().ok();
-                if value.is_none() {
-                    continue;
-                }
-                let index = value.unwrap();
-                if index == i {
-                    index2 = rule_index;
-                    break;
+        }
+    }
+
+    println!("cgq,fnr,kqk,nbc,svm,z15,z23,z39"); // Manual looking through the test cases above
+
+    Ok(())
+}
+
+fn all_differences(
+    swaps: &HashMap<[char; 3], [char; 3]>,
+    number_bits: usize,
+    values: &mut HashMap<[char; 3], Option<bool>>,
+    order: &Vec<Rule>,
+    j: usize,
+) -> usize {
+    let mut total = 0;
+
+    for x0 in 0..2 {
+        for x1 in 0..2 {
+            for y0 in 0..2 {
+                for y1 in 0..2 {
+                    clear_values(values, number_bits);
+                    set_x(values, j, x0);
+                    set_x(values, j + 1, x1);
+                    set_y(values, j, y0);
+                    set_y(values, j + 1, y1);
+                    let bits = calculate_value(values, &order, number_bits + 1, swaps);
+
+                    let sub = x0 + y0 + x1 * 2 + y1 * 2;
+                    let mut compare_bits: Vec<usize> = vec![0; number_bits + 1];
+                    compare_bits[j] = sub & 1;
+                    compare_bits[j + 1] = (sub & 2) >> 1;
+                    compare_bits[j + 2] = (sub & 4) >> 2;
+
+                    let differences = compare_sets(&bits, &compare_bits);
+                    total += differences;
                 }
             }
-            swaps.push(rules[index1].output.clone());
-            swaps.push(rules[index2].output.clone());
-
-            let temp = rules[index2].output;
-            rules[index2].output = rules[index1].output;
-            rules[index1].output = temp;
-            problems[i] = true;
-            outputs[i] = Some(rules[index2].clone());
         }
     }
+    total
+}
 
-    let mut carries: Vec<Option<[char; 3]>> = vec![None; bits + 1];
-    for i in 1..bits {
-        let check_output = input_xors[i].clone().unwrap().output;
-        if outputs[i].clone().unwrap().input1 == check_output {
-            carries[i - 1] = Some(outputs[i].clone().unwrap().input2);
-        } else if outputs[i].clone().unwrap().input2 == check_output {
-            carries[i - 1] = Some(outputs[i].clone().unwrap().input1);
-        } else if !problems[i] {
-            swaps.push(check_output);
-            swaps.push(outputs[i].clone().unwrap().input2);
-        }
-    }
-    swaps.sort();
+fn compare_sets(calculated: &Vec<Option<usize>>, correct: &Vec<usize>) -> usize {
+    let total: usize = calculated
+        .iter()
+        .zip(correct)
+        .map(|item| {
+            if item.0.is_none() {
+                1
+            } else {
+                let value = item.0.unwrap();
+                if value == *item.1 {
+                    0
+                } else {
+                    1
+                }
+            }
+        })
+        .sum();
+    total
+}
 
-    for i in 0..8 {
-        print!("{}{}{},", swaps[i][0], swaps[i][1], swaps[i][2]);
+fn set_x(values: &mut HashMap<[char; 3], Option<bool>>, bit: usize, value: usize) {
+    let x: [char; 3] = [
+        'x',
+        char::from_digit((bit / 10) as u32, 10).unwrap(),
+        char::from_digit((bit % 10) as u32, 10).unwrap(),
+    ];
+    if let Some(val) = values.get_mut(&x) {
+        *val = Some(if value == 0 { false } else { true });
     }
-    println!();
-    return Ok(());
+}
+
+fn set_y(values: &mut HashMap<[char; 3], Option<bool>>, bit: usize, value: usize) {
+    let x: [char; 3] = [
+        'y',
+        char::from_digit((bit / 10) as u32, 10).unwrap(),
+        char::from_digit((bit % 10) as u32, 10).unwrap(),
+    ];
+    if let Some(val) = values.get_mut(&x) {
+        *val = Some(if value == 0 { false } else { true });
+    }
 }
